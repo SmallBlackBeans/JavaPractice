53aed39774405263088f9142fc4cd3e1c4dea2ec
韩承海 <374802597@qq.com>
2018-10-18 23:29:32 +0800

    no message

---
+++ Basic/mybatis-demo/src/test/java/OrderPartyPO.java
@@ -0,0 +1,12 @@
+/**
+ * Description:
+ * User: hanchenghai
+ * Date: 2018/10/18
+ */
+public class OrderPartyPO {
+    boolean isDefault;
+    int     quantity;
+    String  name;
+    String  oid;
+    boolean isNew;
+}
