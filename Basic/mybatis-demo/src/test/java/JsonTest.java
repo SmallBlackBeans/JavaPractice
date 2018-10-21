4523c4e87b05c6a825cb84293dd56f704a137cbb
韩承海 <374802597@qq.com>
2018-10-22 00:39:28 +0800

    no message

--- Basic/mybatis-demo/src/test/java/JsonTest.java
+++ Basic/mybatis-demo/src/test/java/JsonTest.java
@@ -1,4 +1,5 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -12,16 +13,15 @@ public class JsonTest {
         String jsonStr = "{\"code\":10000,\"message\":null,\"data\":{\"orderPartyList\":[{\"isDefault\":true,\"quantity\":0,\"name\":\"浙杭州D32时尚街区\",\"oid\":2897703784,\"isNew\":false},{\"isDefault\":false,\"quantity\":0,\"name\":\"浙杭州文一物美店\",\"oid\":2897704144,\"isNew\":false},{\"isDefault\":false,\"quantity\":0,\"name\":\"浙杭州西城广场\",\"oid\":2897704232,\"isNew\":false}]}}";
         JSONObject json = JSONObject.parseObject(jsonStr);
 
-        //JSONObject data = (JSONObject) json.get("data");
-        //JSONArray orderPartyList = data.getJSONArray("orderPartyList");
-        //for (Object order : orderPartyList) {
-        //    JSONObject or = (JSONObject) order;
-        //    Boolean    isDefault = or.getBoolean("isDefault");
-        //    if (isDefault) {
-        //        System.out.println(or.get("oid"));
-        //    }
-        //}
-
+        JSONObject data           = (JSONObject) json.get("data");
+        JSONArray  orderPartyList = data.getJSONArray("orderPartyList");
+        for (Object order : orderPartyList) {
+            JSONObject or = (JSONObject) order;
+            Boolean    isDefault = or.getBoolean("isDefault");
+            if (isDefault) {
+                System.out.println(or.get("oid"));
+            }
+        }
         //JSON.parseObject(dataStr, OrderData.Data.class);
         OrderData d = JSON.toJavaObject(json,OrderData.class);
         System.out.println(d);
