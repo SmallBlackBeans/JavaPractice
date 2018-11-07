f7bd9287441ee1a197bef63e6e70664a43458584
韩承海 <374802597@qq.com>
2018-11-07 22:32:03 +0800

    no message

--- SpringBoot/SpringBootAll/SpringBoot-JPA/src/main/java/com/hanxiaocu/jpa/Application.java
+++ SpringBoot/SpringBootAll/SpringBoot-JPA/src/main/java/com/hanxiaocu/jpa/Application.java
@@ -1,9 +1,18 @@
 package com.hanxiaocu.jpa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 /**
  * @desc:
  * @author: hanchenghai
  * @date: 2018/11/07 6:02 PM
  */
+@SpringBootApplication
+@EnableJpaAuditing //公共字段自动填充功能
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
