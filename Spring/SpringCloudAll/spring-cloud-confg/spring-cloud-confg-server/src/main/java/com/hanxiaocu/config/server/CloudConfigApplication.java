9e0d803f9c55208bc65b26f18fd2270e9b660b47
韩承海 <374802597@qq.com>
2018-11-12 09:48:55 +0800

    创建配置中心

---
+++ Spring/SpringCloudAll/spring-cloud-confg/spring-cloud-confg-server/src/main/java/com/hanxiaocu/config/server/CloudConfigApplication.java
@@ -0,0 +1,22 @@
+package com.hanxiaocu.config.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @desc:
+ * @author: hanchenghai
+ * @date: 2018/11/12 9:34 AM
+ */
+@SpringBootApplication
+@EnableConfigServer
+@Slf4j
+public class CloudConfigApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigApplication.class,args);
+		log.info("spring-cloud-config-server启动... ");
+	}
+
+}
